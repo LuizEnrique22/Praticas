9787788e11e450995f76f097b7de10e7d48e9e73
LuizEnrique22 <167770780+LuizEnrique22@users.noreply.github.com>
2024-04-22 11:53:51 -0300

    Create AulaJS1.js

---
+++ JS/AulaJS1.js
@@ -0,0 +1,44 @@
+//1) Questão
+
+const n1= 1;
+const n2= 2;
+const n3= 3;
+
+let media = (n1 + n2 + n3)/3;
+
+console.log("A media é:" + media);
+
+
+//2) Questão 
+
+var n11= parseFloat(prompt("Insira o primeiro valor: "));
+var n22= parseFloat(prompt("Insira o segundo valor:  "));
+
+var soma = n11 + n22;
+
+console.log("A soma dos números são:"+ soma);
+
+//3) Questão 
+
+var idade= parseFloat(prompt("Insira sua idade: "));
+var dias= idade*365
+console.log("Sua idade em dias: "+ dias );
+
+//4) Questão 
+
+var nu1 = parseFloat(prompt("Insira seu número: "));
+var quadrado = nu1 * nu1;
+console.log("O seu número quadrado é: "+ quadrado /n);
+ 
+//5) Questão 
+
+var n1 = parseFloat(prompt("Insira o primeiro número:"));
+
+var iup =n1%2
+
+if (iup == 0){
+ console.log("Seu número " + n1 + " é par")
+
+}else{
+console.log("Seu número " + n1 + " é ímpar" )
+}
